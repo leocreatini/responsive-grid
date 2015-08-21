f2fe1253dd821b062c230937ea0baefc2eb828e8
Leo Creatini <creatinileandro@gmail.com>
2015-08-20 20:04:17 -0400

    Smallest Commit Ever
    •  Did not like one word, "like."

--- public/app/home/home.controller.js
+++ public/app/home/home.controller.js
@@ -13,7 +13,7 @@
 		vm.layoutType = 'grid';
 		vm.page = {
 			header: "The Grid",
-			description: "A digital frontier. I tried to imagine what custom responsive grids looked like in the CSS engine. Were they like lanyards in a swimming pool, white-picket fences, or something else entirely?",
+			description: "A digital frontier. I tried to imagine what custom responsive grids looked like in the CSS engine. Were they lanyards in a swimming pool, white-picket fences, or something else entirely?",
 			credit: "Content was sourced from TheGuardian.com."
 		}; //vm.page{}
 
